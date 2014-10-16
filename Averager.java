c9d8cae1dc2bdbcf5f21106e3eaa7f27e3d2befc
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro.local>
2014-10-16 14:21:52 +0100

    GradeAverager (André Purits)

--- Averager.java
+++ Averager.java
@@ -6,11 +6,25 @@
 * @version <date you finished the code>
 *
 */
+import java.util.Scanner;
 
 public class Averager {
 	
 	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
 
+   Scanner scnr = new Scanner(System.in);
+   double totalGrades = 0;
+   int gradeCount = 0;
+   double average = 0;
+   System.out.println("Enter a grade or enter -1 to exit.");
+   double gradeAdded = scnr.nextDouble();
+   while (gradeAdded != -1) {
+      gradeCount += 1;
+      totalGrades += gradeAdded;
+      average = totalGrades/gradeCount;
+      System.out.println("Enter another grade or enter -1 to exit.");
+      gradeAdded = scnr.nextDouble();
+      }
+      System.out.println("The average score is " + average + ".");
+    }
 }
